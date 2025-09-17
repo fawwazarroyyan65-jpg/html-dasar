13c9494d6322d66df0f963d34c0992d02b6de66a
fawwaz arrayyan <fawwazarroyyan65@gmail.com>
2025-09-17 19:40:20 +0700

    html dasar

---
+++ belajar html/coba.py
@@ -0,0 +1,23 @@
+x=5
+y=2
+print(x**y)
+for i in range(1,5,2):
+    print(i)
+data=(1,3,4)
+print(type(data))
+def sapa (nama="daffa"):
+    print("haloo",nama)
+
+sapa()
+data2=[10,20,30]
+print(sum(data2))
+f1=10
+f2=3
+print(f1+f2)
+usia=15
+print(usia >10)
+for b in range(3):
+    print(b)
+daftar=[1,2]
+daftar.append(3)
+print(daftar)
